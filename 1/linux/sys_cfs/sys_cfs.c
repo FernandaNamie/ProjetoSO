89a872d28521caeac6a94fc7f943e98a51db2d17
GabrielKishida <gabriel.kishida@usp.br>
2022-06-14 22:39:05 -0300

    Add vruntime sort to cfs

--- 1/linux/sys_cfs/sys_cfs.c
+++ 1/linux/sys_cfs/sys_cfs.c
@@ -2,18 +2,50 @@
 #include<linux/module.h>
 #include<linux/sched/signal.h>
 
+void switchPlace(struct task_struct* a[], struct task_struct* b[]) {
+    struct task_struct* temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void heapify(struct task_struct* scheduled_task_list[], int n, int i) {
+    int largest = i;  
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+    if (left < n && scheduled_task_list[left]->se.vruntime > scheduled_task_list[largest]->se.vruntime)
+        largest = left;
+    if (right < n && scheduled_task_list[right]->se.vruntime > scheduled_task_list[largest]->se.vruntime )
+        largest = right;
+    if (largest != i) {
+        switchPlace(&scheduled_task_list[i], &scheduled_task_list[largest]);
+        heapify(scheduled_task_list, n, largest);
+    }
+}
+void heapSort(struct task_struct* scheduled_task_list[], int n) {
+    int i;
+    for (i = n / 2 - 1; i >= 0; i--)
+        heapify(scheduled_task_list, n, i);
+
+    for (i = n - 1; i >= 0; i--) {
+        switchPlace(&scheduled_task_list[0], &scheduled_task_list[i]);
+        heapify(scheduled_task_list, i, 0);
+    }
+}
+  
 asmlinkage long sys_cfs(char * virtualRuntime, size_t virtualRuntimeSize)
 {
     struct task_struct* task_list;
-    struct task_struct* p; 
+    struct task_struct* p;
+    struct task_struct* scheduled_task_list[20];
     char* ptr = virtualRuntime;
     int counter = 0;
 
     for_each_process_thread(task_list, p) {
-        ptr += sprintf(ptr, "%d", p->pid);
-        ptr += sprintf(ptr, ":");
-        ptr += sprintf(ptr, "%d", p->se.vruntime);
-        ptr += sprintf(ptr, " ");
+        scheduled_task_list[counter] = p;
+    //    ptr += sprintf(ptr, "%d", p->pid);
+    //    ptr += sprintf(ptr, ":");
+    //    ptr += sprintf(ptr, "%d", p->se.vruntime);
+    //    ptr += sprintf(ptr, " ");
         counter++;
 
         if (counter > 20) {
@@ -22,5 +54,9 @@ asmlinkage long sys_cfs(char * virtualRuntime, size_t virtualRuntimeSize)
     }
     
 fim:
-	return 1;
+    heapSort(scheduled_task_list, 20);
+	return pr_info("pid%d: %llu; \npid%d: %llu; \npid%d: %llu;\n",
+        scheduled_task_list[0]->pid, scheduled_task_list[0]->se.vruntime,
+        scheduled_task_list[1]->pid, scheduled_task_list[1]->se.vruntime,
+        scheduled_task_list[2]->pid, scheduled_task_list[2]->se.vruntime);
 }
