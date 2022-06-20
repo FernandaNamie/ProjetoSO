3783b738213630f0cdd96fadb67141244a290c97
BrunoMovio <bruno.movio@labenu.com.br>
2022-06-20 11:06:42 -0300

    implement panic method

--- 1/initramfs/init.c
+++ 1/initramfs/init.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mount.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include "cfs.h"
+
+#define len(_arr) ((int)((&_arr)[1] - _arr))
+
+void panic(const char *msg) {
+
+	fprintf(stderr, "%s: %s (errno = %d)\n", msg, strerror(errno), errno);
+	exit(-1);
+
+}
 
 static const char * const programs[] = { "/simple_arithmetic", "/write_file" };
 
@@ -7,9 +25,7 @@ int main(){
    char virtualRuntime[300];
    size_t virtualRuntimeSize = 300;
 
-   const int len = sizeof programs / sizeof programs[0];
-
-   for (int i = 0; i < len; i++) {
+   for (int i = 0; i < len(programs); i++) {
       const char *path = programs[i];
       pid_t pid = fork();
       if(pid == -1){
